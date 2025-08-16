43831109406e7e410e60061c0c897c35418a777b
timewatch-dev <timewatchinfocomwebmail@gmail.com>
2025-08-16 15:00:14 +0530

    port name changed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
-const PORT = process.env.SERVER_PORT || 5000;
+const PORT = process.env.SERVER || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
