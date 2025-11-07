de966e278558f0b1f08bfa231f2753cafdc589c2
timewatch-dev <timewatchinfocomwebmail@gmail.com>
2025-11-07 14:39:50 +0530

    changed email port

--- utils/transporter.js
+++ utils/transporter.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
+  port: 587,
   secure: true,
   auth: {
     user: process.env.EMAIL_USER,
